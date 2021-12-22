d771a0ea03ed49baa44b641c21f6fd1983b406b8
ernane <ernanecarvalho89@gmail.com>
2021-12-22 19:49:56 -0300

    sobre e contato

--- pages/sobre.js
+++ pages/sobre.js
@@ -6,9 +6,29 @@ const Sobre = () => {
     return (
         <div>
             <PageTitle title='Sobre' />
-            <h1>Sobre</h1>
-            <div>
-                <Link href='/'>Pagina Incial</Link>
+        
+            <div className='text-center font-bold italic'>
+                <p>A Di Carvalho Web Systems desenvolve soluções tecnólogicas para sua empresa, <br /> 
+                como os sites institucionais que performam dez vezes um site comum, além de utilizar <br /> 
+                técnicas avançadas de SEO para otimizar o ranqueamento no google. 
+                </p>
+                <br /> 
+                <p>
+                Trabalhamos também com produção de lojas virtuais tornando seu negócio uma vitrine na <br /> 
+                rede mundial de computadores(internet-wwww).<br /> 
+                </p>
+                <br /> 
+                <p> Fornececemos serviços de gestão do trafego pago, gerindo as campanhas e otimizando o ranqueamento <br />
+                nas principais redes sociais (Instagram e Facebook) além claro do próprio google, <br /> 
+                tornando sua página as primeiras colocadas nas buscas dos usuários. <br /> 
+                </p>
+                <br /> 
+                <p>
+                Além de tudo ainda criamos toda gestão da sua empresa, o famoso ERP - Sistemas para gestão empresarial <br /> 
+                para que você possa cuidar do seu negócio com tranquilidade.  
+                </p>
+                
+                
             </div>
             
         </div>
